77176ced75e6903b484c6bd8dddebbcb0ba55ceb
Hugh Morison <hugh.morison@hotmail.ca>
2019-11-19 09:27:50 -0500

    implemented sarsa

--- algorithms.py
+++ algorithms.py
@@ -1,24 +1,44 @@
 import numpy as np
-import random
+
 
 class UpdateAlgorithm:
-    def __init__(self, alpha, epsilon, gamma, grid_size, num_actions):
+    def __init__(self, alpha, epsilon, gamma, grid_size, num_actions, goal_state):
         self.alpha = alpha
         self.epsilon = epsilon
         self.gamma = gamma
         self.q = np.random.random(size=(*grid_size, num_actions))
+        self.q[goal_state[0], goal_state[1], :] = 0
 
     def action_selection(self, state):
-        # eps-greedy action selection from q-table
-
+        # epsilon-greedy action selection from q-table
+        actions = self.q[state[0], state[1]]
+        probability = np.array([self.epsilon/len(actions) for _ in actions])
+        probability[np.argmax(actions)] += 1 - self.epsilon
+        action = np.random.choice(len(actions), p=probability)
+        return action
 
 
 class Sarsa(UpdateAlgorithm):
     def __init__(self, alpha, epsilon, gamma, grid_size, num_actions, goal_state):
-        super().__init__(alpha, epsilon, gamma, grid_size, num_actions)
-        self.q[goal_state[0], goal_state[1], :] = 0
+        super().__init__(alpha, epsilon, gamma, grid_size, num_actions, goal_state)
 
-    def update_table(self, reward, state, action, next_state, next_action):
-        self.q[state[0], state[1], action] += self.alpha * (reward +
-                                                            self.gamma * self.q[next_state[0], next_state[1], next_action] -
-                                                            self.q[state[0], state[1], action])
+    def update_table(self, state, action, reward, next_state, next_action):
+        self.q[state[0], state[1], action] += self.alpha * (
+                reward + self.gamma * self.q[next_state[0], next_state[1], next_action] -
+                self.q[state[0], state[1], action])
+
+
+class Qlearning(UpdateAlgorithm):
+    def __init__(self, alpha, epsilon, gamma, grid_size, num_actions, goal_state):
+        super().__init__(alpha, epsilon, gamma, grid_size, num_actions, goal_state)
+
+
+class SarsaLambda(UpdateAlgorithm):
+    def __init__(self, alpha, epsilon, gamma, grid_size, num_actions, goal_state):
+        super().__init__(alpha, epsilon, gamma, grid_size, num_actions, goal_state)
+
+
+class QLambda(UpdateAlgorithm):
+    def __init__(self, alpha, epsilon, gamma, grid_size, num_actions, goal_state):
+        super().__init__(alpha, epsilon, gamma, grid_size, num_actions, goal_state)
+
--- main.py
+++ main.py
@@ -1,10 +1,63 @@
 from gridworld import GridWorld
+from algorithms import Sarsa, Qlearning, SarsaLambda, QLambda
 
-env = GridWorld()
-state = env.reset()
-rl = RL()
-for _ in range(1000):
-    action = rl.action_selection(state, reward) 
-    state, reward, done = env.step(action)
-    if done:
+alg_names = ['Sarsa',
+             'Q-learning',
+             'Sarsa-lambda',
+             'Q-lambda']
+algorithm = alg_names[0]
+
+
+def run_sarsa():
+    env = GridWorld()
+    num_episodes = 1000
+    alpha = 0.05
+    epsilon = 0.05
+    gamma = 0.95
+    grid_size = (env.width, env.height)
+    num_actions = env.num_actions
+    goal_state = env.goal_state
+
+    rl = Sarsa(alpha, epsilon, gamma, grid_size, num_actions, goal_state)
+
+    training = []
+    for _ in range(num_episodes):
         state = env.reset()
+        action = rl.action_selection(state)
+        done = False
+        episode = []
+        while not done:
+            next_state, reward, done = env.step(action)
+            next_action = rl.action_selection(next_state)  # why is action selected when in terminal state?
+            rl.update_table(state, action, reward, next_state, next_action)
+            episode.append((state, action, reward))
+            state = next_state
+            action = next_action
+        training.append(episode)
+
+    return training
+
+
+def run_qlearning():
+    raise NotImplementedError
+
+
+def run_sarsalambda():
+    raise NotImplementedError
+
+
+def run_qlambda():
+    raise NotImplementedError
+
+
+if __name__ == '__main__':
+    if algorithm is 'Sarsa':
+        run_sarsa()
+    elif algorithm is 'Q-learning':
+        run_qlearning()
+    elif algorithm is 'Sarsa-lambda':
+        run_sarsalambda()
+    elif algorithm is 'Q-lambda':
+        run_qlambda()
+    else:
+        print('No Algorithm Selected')
