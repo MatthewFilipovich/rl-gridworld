489e7765deb86d3bf025a5b29d1c0a95e1774dd9
Hugh Morison <hugh.morison@hotmail.ca>
2019-11-21 13:07:57 -0500

    multiple changes

--- algorithms.py
+++ algorithms.py
@@ -2,18 +2,26 @@ import numpy as np
 
 
 class UpdateAlgorithm:
-    def __init__(self, alpha=0.05, epsilon=0.01, gamma=0.5, state_space_size=(10, 7), num_actions=4, goal_state=(7, 3)):
+    def __init__(self, env=None, alpha=0.05, epsilon=0.01, gamma=0.5, table_init='zeros', ):
+        assert env is not None
+        self.env = env
+        state_space_size = env.size
+        num_actions = env.num_actions
+        goal_state = env.goal_state
         self.alpha = alpha
         self.epsilon = epsilon
         self.gamma = gamma
-        self.q = np.random.random(size=(*state_space_size, num_actions))
-        self.q[goal_state[0], goal_state[1], :] = 0
+        if table_init == 'random':
+            self.q = np.random.random(size=(*state_space_size, num_actions))
+            self.q[goal_state[0], goal_state[1], :] = 0
+        elif table_init == 'zeros':
+            self.q = np.zeros(shape=(*state_space_size, num_actions))
 
-    def action_selection(self, state):
+    def action_selection(self, state, epsilon):
         # epsilon-greedy action selection from q-table
         actions = self.q[state[0], state[1]]
-        probability = np.array([self.epsilon/len(actions) for _ in actions])
-        probability[np.argmax(actions)] += 1 - self.epsilon
+        probability = np.array([epsilon/len(actions) for _ in actions])
+        probability[np.argmax(actions)] += 1 - epsilon
         action = np.random.choice(len(actions), p=probability)
         return action
 
@@ -27,18 +35,89 @@ class Sarsa(UpdateAlgorithm):
                 reward + self.gamma * self.q[next_state[0], next_state[1], next_action] -
                 self.q[state[0], state[1], action])
 
+    def train(self, num_episodes, epsilon):
+        training = []
+        for i in range(1, num_episodes + 1):
+            self.epsilon = epsilon / i
+            state = self.env.reset()
+            action = self.action_selection(state, self.epsilon)
+            done = False
+            episode = []
+            print('Episode {}:'.format(i))
+            while not done:
+                next_state, reward, done = self.env.step(action)
+                next_action = self.action_selection(next_state, self.epsilon)  # why is action selected when in terminal state?
+                self.update_table(state, action, reward, next_state, next_action)
+                episode.append((state, action, reward))
+                state = next_state
+                action = next_action
+            training.append(episode)
+            print('\tTook {} moves to reach the goal'.format(len(episode)))
+        return training
+
 
 class QLearning(UpdateAlgorithm):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+    def update_table(self, state, action, reward, next_state):
+        self.q[state[0], state[1], action] += self.alpha * (
+                reward + self.gamma * max(self.q[next_state[0], next_state[1], :]) -
+                self.q[state[0], state[1], action])
+
+    def train(self, num_episodes, epsilon):
+        training = []
+        for i in range(1, num_episodes + 1):
+            self.epsilon = epsilon / i
+            state = self.env.reset()
+            done = False
+            episode = []
+            print('Episode {}:'.format(i))
+            while not done:
+                action = self.action_selection(state, self.epsilon)
+                next_state, reward, done = self.env.step(action)
+                self.update_table(state, action, reward, next_state)
+                episode.append((state, action, reward))
+                state = next_state
+            training.append(episode)
+            print('\tTook {} moves to reach the goal'.format(len(episode)))
+        return training
 
 
 class SarsaLambda(UpdateAlgorithm):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def train(self, num_episodes):
+        training = []
+        for i in range(1, num_episodes + 1):
+            state = self.env.reset()
+            action = self.action_selection(state, self.epsilon)
+            done = False
+            episode = []
+            print('Episode {}:'.format(i))
+            z = np.zeros()
+            while not done:
+                next_state, reward, done = self.env.step(action)
+                delta = reward
+                for j in F(state,action):
+                    delta -= w
+                    z += 1
+                if done:
+                    w += self.alpha * delta * z
+                    break
+                next_action = self.action_selection(next_state, 0.0001)
+                for j in F(next_state,next_action):
+                    delta += self.gamma * w
+                w += self.alpha * delta * z
+                z = self.gamma * lambd * z
+                state = next_state
+                action = next_action
+
 
 class QLambda(UpdateAlgorithm):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
+    def train(self):
+        raise NotImplementedError
--- gridworld.py
+++ gridworld.py
@@ -3,20 +3,24 @@
 """
 import numpy as np
 import random
+import curses
+import time
 
 
 class GridWorld:
     """Grid World simulation."""
 
-    def __init__(self, initial_state=(0, 3), goal_state=(7, 3), stochastic_wind=False):
+    def __init__(self, initial_state=(0, 3), goal_state=(7, 3), stochastic_wind=False, print_board=False):
         self.stochastic_wind = stochastic_wind
         self.wind = np.array([0, 0, 0, 1, 1, 1, 2, 2, 1, 0])
         self._initial_state = initial_state
         self.state = np.array(initial_state)
+        self.print_board = print_board
 
         # information about the board
         self.width = len(self.wind)
         self.height = 7
+        self.size = (self.width, self.height)
         self.goal_state = goal_state
         self.num_actions = 4
         self.moves = np.array([[x, y] for x, y in zip([1, 0, -1, 0],
@@ -25,6 +29,10 @@ class GridWorld:
             self._num_actions = 8
             self.moves = np.array([[x, y] for x, y in zip([1, 1, 0, -1, -1, -1, 0, 1],
                                                           [0, 1, 1, 1, 0, -1, -1, -1])])  # kings moves
+        if print_board:
+            self.stdscr = curses.initscr()
+            curses.noecho()
+            curses.cbreak()
 
     def step(self, move):
         """Move the agent in Grid World.
@@ -37,11 +45,36 @@ class GridWorld:
         wind = self._apply_wind()  # get wind from original state
         if self._check_move(self.state + self.moves[move]):  # would move leave the grid
             self.state += self.moves[move]
-        if self._check_move(self.state + wind):  # would wind push player off the grid
-            self.state += wind
+        self.state += wind
+        if not self._check_move(self.state):  # wind pushed player off the grid
+            if self.state[1] >= self.height:
+                self.state[1] = self.height - 1
+            elif self.state[1] < 0:
+                self.state[1] = 0
+        if self.print_board:
+            self.print_grid()
         if self._game_over():
+            if self.print_board:
+                curses.nocbreak()
+                curses.echo()
+                curses.endwin()
             return self.state, 1, True  # Reward of 1
         return self.state, -1, False  # Game not over: reward -1
+
+    def print_grid(self):
+        string = ''
+        for j in range(self.size[1]-1, -1, -1):
+            for i in range(self.size[0]):
+                if i == self.goal_state[0] and j == self.goal_state[1]:
+                    string += ' * '
+                elif i == self.state[0] and j == self.state[1]:
+                    string += ' o '
+                else:
+                    string += '   '
+            string += '\n'
+        self.stdscr.addstr(0, 0, string)
+        self.stdscr.refresh()
+        time.sleep(0.05)
 
     def reset(self):
         """Resets state to original position and returns state"""
